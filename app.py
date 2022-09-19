3cb510d34c067ff9fdb44f8c3f667c511c26539b
dpetesic <dpetesic@student.unizd.hr>
2022-09-19 19:50:28 +0200

    db intergation implement

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import sqlite3 as sql
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    language = db.Column(db.String(30), nullable=False)
+
 
 @app.route('/')
 @app.route('/home/')
@@ -14,3 +27,13 @@ def search():
 @app.route('/signUp/')
 def signUp():
     return render_template("signUp.html")
+
+@app.route('/confirm/', methods=['POST'])
+def confirm(name, email, language):
+
+    cur = sql.connect('users.db')
+    c = cur.cursor()
+    c.execute("INSERT INTO User (name, email, language) VALUES (%s, %s, %s)" %(name, email, language))
+    cur.commit()
+
+    return render_template("confirm.html")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Donat\Desktop\Web\projekt-1-linkedin\venv
+set VIRTUAL_ENV=C:\Users\Donat\Desktop\Zavrsni Web\zavrsni-projekt-group-linkedin\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
